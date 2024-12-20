5566e71113a5f280556ec20867d740d140172e19
boopathiBM <boopathibm19.com>
2024-12-20 14:12:35 +0530

    added addmethod

--- Calculator.java
+++ Calculator.java
@@ -1,14 +1,26 @@
 import java.util.Scanner;
 
+
 public class Calculator{
+    
+    private int add( int a, int b){
+        return a+b;
+    }
+
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         System.out.println("enter integer 1: ");
         int a = scanner.nextInt();
         System.out.println("enter integer 2: ");
         int b = scanner.nextInt();
-        int c = a+b;
-        System.out.println("sum of two intergers: "+ c);
+        // int c = a+b;
+        // System.out.println("sum of two intergers: "+ c);
+        Calculator c1 = new Calculator();
+        
+        int result = c1.add(a,b);
+        System.out.println("sum of two integers: " + result);
+        
+
 
     }
 }
