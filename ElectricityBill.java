fa86b049ac5f8a9cf8fd092cb2d879f6153e03f1
boopathiBM <boopathibm19.com>
2024-12-31 09:55:25 +0530

    formatted  code

--- ElectricityBill.java
+++ ElectricityBill.java
@@ -1,49 +1,49 @@
 import java.util.Scanner;
 
-public class ElectricityBill{
-    public static void main(String[] args){
+public class ElectricityBill {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("enter the unit: ");
         int unit = scanner.nextInt();
         ElectricityBill electricitybill = new ElectricityBill();
         int bill = electricitybill.calculateBill(unit);
-        System.out.println("bill amount for "+ unit + " unit is: "+ bill);
+        System.out.println("bill amount for " + unit + " unit is: " + bill);
         bill = electricitybill.calBill(unit);
-        System.out.println(" method 2 bill amount for "+ unit + " unit is: "+ bill);
+        System.out.println(" method 2 bill amount for " + unit + " unit is: " + bill);
 
     }
-    public int calculateBill(int unit){
-        int bill =0;
-        if (unit <= 0){
-            return 0;
-        }
-        if(unit <= 100){
-            bill= unit * 5;
-            return bill;
-        }
-        if(unit <=200){
-        bill = 500 +( (unit - 100) * 7 );
-        return bill;
-        }
-        bill = 1200 + ((unit -200) * 10);
-        return bill;
 
-        
-    }
-    public int calBill(int unit){
+    public int calculateBill(int unit) {
         int bill = 0;
-        for(int i=1;i<=unit;i++){
-            if(i<=100){
+        if (unit <= 0) {
+            bill = 0;
+        } else if (unit <= 100) {
+            bill = unit * 5;
+
+        } else if (unit <= 200) {
+            bill = 500 + ((unit - 100) * 7);
+        } else {
+            bill = 1200 + ((unit - 200) * 10);
+        }
+
+        return bill;
+
+    }
+
+    public int calBill(int unit) {
+        int bill = 0;
+        for (int i = 1; i <= unit; i++) {
+            if (i <= 100) {
                 bill += 5;
             }
-            if(i>100 && i<= 200){
-                bill +=7;
+            if (i > 100 && i <= 200) {
+                bill += 7;
             }
-            if(i>200){
-                bill +=10;
+            if (i > 200) {
+                bill += 10;
             }
         }
         return bill;
-        
+
     }
 }
