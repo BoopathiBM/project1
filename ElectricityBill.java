d9076a9501ad7396cbf12bd42d53ecacdef18129
boopathiBM <boopathibm19.com>
2024-12-31 09:50:05 +0530

    method to avoid loop concept

--- ElectricityBill.java
+++ ElectricityBill.java
@@ -8,8 +8,29 @@ public class ElectricityBill{
         ElectricityBill electricitybill = new ElectricityBill();
         int bill = electricitybill.calculateBill(unit);
         System.out.println("bill amount for "+ unit + " unit is: "+ bill);
+        bill = electricitybill.calBill(unit);
+        System.out.println(" method 2 bill amount for "+ unit + " unit is: "+ bill);
+
     }
     public int calculateBill(int unit){
+        int bill =0;
+        if (unit <= 0){
+            return 0;
+        }
+        if(unit <= 100){
+            bill= unit * 5;
+            return bill;
+        }
+        if(unit <=200){
+        bill = 500 +( (unit - 100) * 7 );
+        return bill;
+        }
+        bill = 1200 + ((unit -200) * 10);
+        return bill;
+
+        
+    }
+    public int calBill(int unit){
         int bill = 0;
         for(int i=1;i<=unit;i++){
             if(i<=100){
